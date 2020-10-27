fe46a695a0fc6cc70b2936315912239ffaa0913c
Mykyta Belimenko <nikita.belimenko@enehano.cz>
2020-10-27 21:02:05 +0100

    init project

--- src/main/java/com/memsource/assignments/config/RestConfigurer.java
+++ src/main/java/com/memsource/assignments/config/RestConfigurer.java
@@ -1,6 +1,7 @@
 package com.memsource.assignments.config;
 
 import com.memsource.assignments.project.Project;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,7 +11,11 @@ public class RestConfigurer implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedHeaders("*");
         config.exposeIdsFor(Project.class);
     }
-
 }
